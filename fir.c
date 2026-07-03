6b4fbe4e19d54f738317fd568c5e13abaee8f92e
Nina Dragićević <grozdje2706@gmail.com>
2026-07-03 13:25:32 +0200

    Restore FIR filter functions and includes

--- fir.c
+++ fir.c
@@ -10,17 +10,14 @@ Int16 fir_basic(Int16 input, Int16* coeffs, Int16 *history, Uint16 n_coeff)
 	Int16 i;
 	Int32 ret_val = 0;
 
-	/* shift delay line */
 	for (i = n_coeff - 2; i >= 0; i--)
 	{
 		history[i + 1] = history[i];
 	}
 
-	/* store input at the beginning of the delay line */
 	history[0] = input;
 
 
-	/* calc FIR */
 	for (i = 0; i < n_coeff; i++)
 	{
 		ret_val = _smac(ret_val, coeffs[i]>> HEADROOM_BITS, history[i]);
@@ -35,28 +32,26 @@ Int16 fir_circular(Int16 input, Int16 *coeffs, Int16 *history, Uint16 n_coeff, U
     Uint16 state;
     Int32 ret_val;
 
-    state = *p_state;               /* copy the filter's state to a local */
+    state = *p_state;              
 
 
-	/* store input at the beginning of the delay line */
     history[state] = input;
 	if (++state >= n_coeff)
-	{         /* incr state and check for wrap */
+	{        
 		state = 0;
 	}
 
-	/* calc FIR and shift data */
 	ret_val = 0;
 	for (i = n_coeff - 1; i >= 0; i--)
 	{
 		ret_val = _smac(ret_val, coeffs[i]>> HEADROOM_BITS, history[state]);
-		if (++state >= n_coeff) /* incr state and check for wrap */
+		if (++state >= n_coeff) 
 		{
 			state = 0;
 		}
 	}
 
-    *p_state = state;               /* return new state to caller */
+    *p_state = state;              
 
     return (Int16)(ret_val >> 16);
 }
